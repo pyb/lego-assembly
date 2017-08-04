94c23013e7f53664af404045571c72ad99730cf4
Khalid <khalid@alum.recurse.com>
2017-08-05 00:14:37 +0200

    Add model

---
+++ model.js
@@ -0,0 +1,29 @@
+// graph representation of Lego bricks
+const H = require("./helpers");
+
+module.exports = {
+    bricks:[{ ref: 0,
+              pos: [0, 0, 0],
+              angle: 0,
+              localStuds: [[0, -1, 0], [1, -1, 0]],
+              localHoles: [[0, 0, 0], [1, 0, 0]],
+              worldStuds: [H.createCVarPos(), H.createCVarPos()],
+              worldHoles: [H.createCVarPos(), H.createCVarPos()]
+            },
+            { ref: 1,
+              pos: H.createCVarPos(),
+              angle: 0,
+              localStuds: [[0, -1, 0], [1, -1, 0]],
+              localHoles: [[0, 0, 0], [1, 0, 0]],
+              worldStuds: [H.createCVarPos(), H.createCVarPos()],
+              worldHoles: [H.createCVarPos(), H.createCVarPos()]
+            },
+            { ref: 2,
+              pos: H.createCVarPos(),
+              angle: 0,
+              localStuds: [[0, -1, 0], [1, -1, 0]],
+              localHoles: [[0, 0, 0], [1, 0, 0]],
+              worldStuds: [H.createCVarPos(), H.createCVarPos()],
+              worldHoles: [H.createCVarPos(), H.createCVarPos()]
+            }]
+};
