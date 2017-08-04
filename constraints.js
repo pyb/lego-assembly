8f00f8c30a9383a99093019203371abadc090f1c
Khalid <khalid@alum.recurse.com>
2017-08-04 17:52:15 +0200

    Change module alias

--- constraints.js
+++ constraints.js
@@ -1,8 +1,8 @@
 // API exposed by the cassowaray obj
-const c = require("cassowary");
-const solver = new c.SimplexSolver();
-const x = new c.Variable({ value: 1000 });
-const eq2 = new c.Equation(x, 42);
+const cas = require("cassowary");
+const solver = new cas.SimplexSolver();
+const x = new cas.Variable({ value: 1000 });
+const eq2 = new cas.Equation(x, 42);
 solver.addConstraint(eq2);
 solver.resolve();
 console.log(x.value);
