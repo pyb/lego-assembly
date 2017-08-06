7434629a7749fa425db1f503f68bd240f72bfa2c
Khalid <khalid@alum.recurse.com>
2017-08-06 20:48:44 +0200

    Change variable name

--- model.js
+++ model.js
@@ -1,29 +1,29 @@
 // graph representation of Lego bricks
-const H = require("./helpers");
+const helpers = require("./helpers");
 
 module.exports = {
     bricks: [{ ref: 0,
                pos: [0, 0, 0],
                angle: 0,
                localStuds: [[0, -1, 0], [1, -1, 0]],
-               localHoles: [[0, 0, 0], [1, 0, 0]],
-               worldStuds: [H.createCVarPos(), H.createCVarPos()],
-               worldHoles: [H.createCVarPos(), H.createCVarPos()]
+               localhelpersoles: [[0, 0, 0], [1, 0, 0]],
+               worldStuds: [helpers.createCVarPos(), H.createCVarPos()],
+               worldhelpersoles: [H.createCVarPos(), H.createCVarPos()]
              },
              { ref: 1,
-               pos: H.createCVarPos(),
+               pos: helpers.createCVarPos(),
                angle: 0,
                localStuds: [[0, -1, 0], [1, -1, 0]],
-               localHoles: [[0, 0, 0], [1, 0, 0]],
-               worldStuds: [H.createCVarPos(), H.createCVarPos()],
-               worldHoles: [H.createCVarPos(), H.createCVarPos()]
+               localhelpersoles: [[0, 0, 0], [1, 0, 0]],
+               worldStuds: [helpers.createCVarPos(), H.createCVarPos()],
+               worldhelpersoles: [H.createCVarPos(), H.createCVarPos()]
              },
              { ref: 2,
-               pos: H.createCVarPos(),
+               pos: helpers.createCVarPos(),
                angle: 0,
                localStuds: [[0, -1, 0], [1, -1, 0]],
-               localHoles: [[0, 0, 0], [1, 0, 0]],
-               worldStuds: [H.createCVarPos(), H.createCVarPos()],
-               worldHoles: [H.createCVarPos(), H.createCVarPos()]
+               localhelpersoles: [[0, 0, 0], [1, 0, 0]],
+               worldStuds: [helpers.createCVarPos(), H.createCVarPos()],
+               worldhelpersoles: [H.createCVarPos(), H.createCVarPos()]
              }]
 };
