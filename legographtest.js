61ab9c7b0fb9eb42f998b26a38c227e7a5122b2d
Khalid <khalid@alum.recurse.com>
2017-08-07 16:46:19 +0200

    Refactor duplicated code into loops

--- legographtest.js
+++ legographtest.js
@@ -1,62 +1,31 @@
 "use strict";
 
 const cas = require("cassowary");
-const H = require("./helpers");
+const helpers = require("./helpers");
 const G = require("./model");
 
 const addLinkToCSolver = (solver, nBrickA, nStudA, nBrickB, nHoleB) => {
     const studPos = G.bricks[nBrickA].worldStuds[nStudA];
     const holePos = G.bricks[nBrickB].worldHoles[nHoleB];
-    H.equaliseTwoArrays(solver, studPos, holePos);
+    helpers.equaliseTwoArrays(solver, studPos, holePos);
 };
 
 // refactor to reduce repititiveness
 const addWorldRelationToCSolver = (solver, nBrick) => {
     const brick = G.bricks[nBrick];
-
-    const worldStudA = brick.worldStuds[0];
-    const localStudA = brick.localStuds[0];
-    const worldStudB = brick.worldStuds[1];
-    const localStudB = brick.localStuds[1];
-
-    const worldHoleA = brick.worldHoles[0];
-    const localHoleA = brick.localHoles[0];
-    const worldHoleB = brick.worldHoles[1];
-    const localHoleB = brick.localHoles[1];
-
     const brickPos = brick.pos;
 
-    // stud A's X, Y, Z co-ordinates
-    solver.addConstraint(new cas.Equation(worldStudA[0],
-                                          cas.plus(localStudA[0], brickPos[0])));
-    solver.addConstraint(new cas.Equation(worldStudA[1],
-                                          cas.plus(localStudA[1], brickPos[1])));
-    solver.addConstraint(new cas.Equation(worldStudA[2],
-                                          cas.plus(localStudA[2], brickPos[2])));
-    // stud B's X, Y, Z co-ordinates
-    solver.addConstraint(new cas.Equation(worldStudB[0],
-                                          cas.plus(localStudB[0], brickPos[0])));
-    solver.addConstraint(new cas.Equation(worldStudB[1],
-                                          cas.plus(localStudB[1], brickPos[1])));
-    solver.addConstraint(new cas.Equation(worldStudB[2],
-                                          cas.plus(localStudB[2], brickPos[2])));
-
-    // hole A's X, Y, Z co-ordinates
-    solver.addConstraint(new cas.Equation(worldHoleA[0],
-                                          cas.plus(localHoleA[0], brickPos[0])));
-    solver.addConstraint(new cas.Equation(worldHoleA[1],
-                                          cas.plus(localHoleA[1], brickPos[1])));
-    solver.addConstraint(new cas.Equation(worldHoleA[2],
-                                          cas.plus(localHoleA[2], brickPos[2])));
-
-    // hole B's X, Y, Z co-ordinates
-    solver.addConstraint(new cas.Equation(worldHoleB[0],
-                                          cas.plus(localHoleB[0], brickPos[0])));
-    solver.addConstraint(new cas.Equation(worldHoleB[1],
-                                          cas.plus(localHoleB[1], brickPos[1])));
-    solver.addConstraint(new cas.Equation(worldHoleB[2],
-                                          cas.plus(localHoleB[2], brickPos[2])));
-};
+    // process stud A and stud B
+    for (let j = 0 ; j < 2 ; j+=1) {
+    // stud j's X, Y, Z co-ordinates
+        for (let i = 0 ; i < 3 ; i+=1) {
+            solver.addConstraint(new cas.Equation(brick.worldStuds[j][i],
+                                                  cas.plus(brick.localStuds[j][i], brickPos[i])));
+            solver.addConstraint(new cas.Equation(brick.worldHoles[j][i],
+                                                  cas.plus(brick.localHoles[j][i], brickPos[i])));
+        }
+    }
+}
 
 const testConstraintSolver = () => {
     const solver = new cas.SimplexSolver();
@@ -75,5 +44,4 @@ const testConstraintSolver = () => {
     console.log("Brick 1 position : ", G.bricks[1].pos);
     console.log("Brick 2 position : ", G.bricks[2].pos);
 };
-
 testConstraintSolver();
--- model.js
+++ model.js
@@ -8,24 +8,24 @@ module.exports = {
                pos: [0, 0, 0],
                angle: 0,
                localStuds: [[0, -1, 0], [1, -1, 0]],
-               localhelpersoles: [[0, 0, 0], [1, 0, 0]],
-               worldStuds: [helpers.createCVarPos(), H.createCVarPos()],
-               worldhelpersoles: [H.createCVarPos(), H.createCVarPos()]
+               localHoles: [[0, 0, 0], [1, 0, 0]],
+               worldStuds: [helpers.createCVarPos(), helpers.createCVarPos()],
+               worldHoles: [helpers.createCVarPos(), helpers.createCVarPos()]
              },
              { ref: 1,
                pos: helpers.createCVarPos(),
                angle: 0,
                localStuds: [[0, -1, 0], [1, -1, 0]],
-               localhelpersoles: [[0, 0, 0], [1, 0, 0]],
-               worldStuds: [helpers.createCVarPos(), H.createCVarPos()],
-               worldhelpersoles: [H.createCVarPos(), H.createCVarPos()]
+               localHoles: [[0, 0, 0], [1, 0, 0]],
+               worldStuds: [helpers.createCVarPos(), helpers.createCVarPos()],
+               worldHoles: [helpers.createCVarPos(), helpers.createCVarPos()]
              },
              { ref: 2,
                pos: helpers.createCVarPos(),
                angle: 0,
                localStuds: [[0, -1, 0], [1, -1, 0]],
-               localhelpersoles: [[0, 0, 0], [1, 0, 0]],
-               worldStuds: [helpers.createCVarPos(), H.createCVarPos()],
-               worldhelpersoles: [H.createCVarPos(), H.createCVarPos()]
+               localHoles: [[0, 0, 0], [1, 0, 0]],
+               worldStuds: [helpers.createCVarPos(), helpers.createCVarPos()],
+               worldHoles: [helpers.createCVarPos(), helpers.createCVarPos()]
              }]
 };
