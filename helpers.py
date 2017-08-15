0b68599aceeaabe47577b48ee3af6b4fde0cec8f
Khalid <khalid@alum.recurse.com>
2017-08-15 13:39:17 +0200

    Add helpers and a model

---
+++ helpers.py
@@ -0,0 +1,28 @@
+counter = 0
+
+class Variable:
+    def __init__(self):
+        global counter
+        self._index = counter
+        counter += 1
+    def get_index(self):
+        return self._index
+
+    def to_string(self):
+        return 'V[ ' + str(self._index) + ']'
+
+def createVarPos():
+    x = Variable()
+    y = Variable()
+    z = Variable()
+    return [x, y, z]
+
+def equaliseTwoVars(x, y, relations):
+    fn_str = 'lambda V: ' + x.to_string() + ' - ' + y.to_string()
+    fn = eval(fn_str)
+    relations.append(fn)
+
+def equaliseTwoLists(xs, ys, relations):
+    l = len(xs)
+    for i in range(0, l):
+        equaliseTwoVars(xs[i], ys[i], relations)
---
+++ model.py
@@ -0,0 +1,32 @@
+import helpers
+
+bricks = [
+    {
+        'ref': 0,
+        'pos': helpers.createVarPos(),
+        'angle': 0,
+        'localStuds': [[0, -1, 0], [1, -1, 0]],
+        'localHoles': [[0, 0, 0], [1, 0, 0]],
+        'worldStuds': [helpers.createVarPos(), helpers.createVarPos()],
+        'worldHoles': [helpers.createVarPos(), helpers.createVarPos()]
+    },
+    {
+        'ref': 1,
+        'pos': helpers.createVarPos(),
+        'angle': 0,
+        'localStuds': [[0, -1, 0], [1, -1, 0]],
+        'localHoles': [[0, 0, 0], [1, 0, 0]],
+        'worldStuds': [helpers.createVarPos(), helpers.createVarPos()],
+        'worldHoles': [helpers.createVarPos(), helpers.createVarPos()]
+    },
+    {
+        'ref': 2,
+        'pos': helpers.createVarPos(),
+        'angle': 0,
+        'localStuds': [[0, -1, 0], [1, -1, 0]],
+        'localHoles': [[0, 0, 0], [1, 0, 0]],
+        'worldStuds': [helpers.createVarPos(), helpers.createVarPos()],
+        'worldHoles': [helpers.createVarPos(), helpers.createVarPos()]
+    }
+]
+
