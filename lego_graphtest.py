5c9afdd8c2b2496ba70ee3d144fe3046f447c99c
Khalid <khalid@alum.recurse.com>
2017-08-15 13:38:45 +0200

    Port lego graph test to Python

--- lego_graphtest.py
+++ lego_graphtest.py
@@ -1,13 +1,46 @@
 import numpy as np
 import scipy.optimize
+import helpers as h
+import model as m
 
-def F(x):
-    alpha = 1.0
-    beta = x[0]
-    gamma = x[1]
-    f1 = 6 * np.cos(alpha) + 10 * np.cos(gamma) - 6 * np.cos(beta) - 8
-    f2 = 6 * np.sin(alpha) + 10 * np.sin(gamma) - 6 * np.sin(beta)
-    return [f1, f2]
+relations = []
 
-x = scipy.optimize.broyden1(F, [0, 0], f_tol=1e-14)
-print(x)
+def addLinkToSolver(nBrickA, nStudA, nBrickB, nHoleB):
+    studPos = m.bricks[nBrickA].worldStuds[nStudA]
+    holePos = m.bricks[nBrickB].worldHoles[nHoleB]
+    h.equaliseTwoLists(studPos, holePos)
+
+def addWorldRelationToSolver(relations, nBrick):
+    brick = m.bricks[nBrick]
+    brickPos = brick.pos
+    for j in range(0, 2):
+        worldStud = brick.worldStuds[j]
+        worldHole = brick.worldHoles[j]
+        localStud = brick.localStuds[j]
+        localHole = brick.localHoles[j]
+        for i in range(0, 3):
+            fn_str = 'lambda V: ' + worldStud[i].to_string() + ' - ' + localStud[i].to_string() + ' - ' + brickPos[i]
+            fn = eval(fn_str)
+            relations.append(fn)
+
+def F(V):
+    return [f(V) for f in relations]
+
+def testConstraintSolver():
+    addWorldRelationToSolver(relations, 0)
+    addWorldRelationToSolver(relations, 1)
+    addWorldRelationToSolver(relations, 2)
+
+    addLinkToSolver(0, 1, 1, 0)
+    addLinkToSolver(1, 0, 2, 0)
+    addLinkToSolver(1, 1, 2, 1)
+
+    # put first brick into [0, 0, 0]
+    for i in range(0, 3):
+        fn_str = 'lambda V: ' + m.bricks[0].pos[i].to_string()
+        fn = eval(fn_str)
+        relations.append(fn)
+    V = scipy.optimize.broyden1(F, [0] * h.counter, f_tol=1e-14)
+    print(V)
+
+testConstraintSolver()
