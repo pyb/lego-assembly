ea93de1b6d6b038f9b260fc07ddd77f8a63029ee
Khalid <khalid@alum.recurse.com>
2017-08-24 15:46:29 +0200

    Write ideas for extracting values from variables in environment

--- v2/compute.js
+++ v2/compute.js
@@ -3,6 +3,7 @@
 const h = require('./helpers2');
 
 // Relation object contains a function, as well as all the variables involved (name and location in the model and / or in the master list of all variables)
+
 // Use extractor functions to compose with relations, to obtain relevant variables from the master array of all variables.
 
 let nextIndex = h.makeCounter();
