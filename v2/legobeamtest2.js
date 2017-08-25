5b3f0ba501b279b0ab33aea5eec5dac531b6d55b
Khalid <khalid@alum.recurse.com>
2017-08-25 17:19:32 +0200

    Improve test model

--- v2/legobeamtest2.js
+++ v2/legobeamtest2.js
@@ -5,17 +5,33 @@ const h = require('./helpers2');
 const c = require('./compute');
 const g = require('./geometry_relations');
 
+let beamA;
+let beamB;
+
 const printFunctions = () => {
-    g.functions_to_solve.map((f) => { console.log(f.toString); });
+    g.functions_to_solve.map((f) => { console.log(f.toString()); });
 };
 
-const buildModel () => {
-    const beamA = new m.Beam(7);
-    const beamB = new m.Beam(5);
+const buildModel = () => {
+    beamA = new m.Beam(7);
+    beamB = new m.Beam(5);
 
-    // add six relations
+    g.addZplus1constraint(beamA, beamB);
+    g.fixBeamPosition(beamA, [0, 0, 0]);
+    g.fixBeamAngle(beamA, 1.0);
+    g.fixBeamAngle(beamB, 2.0);
 
+    g.linkConnectors(beamA.connectors[0], beamB.connectors[1]);
 
+    beamA.connectors.map(g.relateLocalToWorld);
+    beamB.connectors.map(g.relateLocalToWorld);
 
+    printFunctions();
+};
 
+module.exports = {
+    printFunctions: printFunctions,
+    buildModel: buildModel,
+    beamA: beamA,
+    beamB: beamB
 };
