5ec205f23e339812cbf1eb4d9598a16da75556ae
Khalid <khalid@alum.recurse.com>
2017-08-25 15:30:16 +0200

    Add geometry relations

---
+++ v2/geometry_relations.js
@@ -0,0 +1,80 @@
+let functions_to_solve = [];
+
+const generateExtractFn = (...variables) =>
+          (env) =>
+              variables.map((v) =>
+                            c.lookUp(v, env));
+
+const makeVariablesEqual = (v1, v2) => {
+    const f = (a, b) => a - b;
+    const extractv1Andv2 = generateExtractFn(v1, v2);
+    functions_to_solve.push((env) => f(...extractv1Andv2(env)));
+};
+
+// zA = zB
+const addEqualZconstraint = (beamA, beamB) => {
+    const zA = beamA.pos.z;
+    const zB = beamB.pos.z;
+    makeVariablesEqual(zA, zB);
+};
+
+// zA = zB + 1
+const addZplus1constraint = (beamA, beamB) => {
+    const zA = beamA.pos.z;
+    const zB = beamB.pos.z;
+
+    const f = (vZa, vZb) => vZa - vZb - 1;
+
+    const extractZaAndZb = generateExtractFn(zA, zB);
+    functions_to_solve.push((env) => f(...extractZaAndZb(env)));
+};
+
+// beam position = [3, 3, 0]
+const fixBeamPosition = (beam, [X, Y, Z]) => {
+    const p = makeConstantPoint(X, Y, Z);
+    makeVariablesEqual(beam.x, p.x);
+    makeVariablesEqual(beam.y, p.y);
+    makeVariablesEqual(beam.z, p.z);
+};
+
+const fixBeamAngle = (beam, angleValue) => {
+    const f = (v) => v - angleValue;
+    const extractAngle = generateExtractFn(beam.angle);
+    functions_to_solve.push((env) => f(...extractAngle(env)));
+};
+
+const linkConnectorectors = (connector1, connector2) => {
+    connector1.targets = [connector2];
+    connector2.targets = [connector1];
+    makeVariablesEqual(connector1.worldPos.x, connector2.worldPos.x);
+    makeVariablesEqual(connector1.worldPos.y, connector2.worldPos.y);
+};
+
+const relateLocalToWorld = (connector) => {
+    const angle = connector.brick.angle;
+    const { x: xBeam, y: yBeam, z: zBeam } = connector.brick.pos;
+    const { x: xLocal, y: yLocal, z: zLocal } = connector.localPos;
+    const { x: xWorld, y: yWorld, z: zWorld } = connector.worldPos;
+
+    const f1 = (zWorld, zLocal, zBeam) => zBeam + zLocal - zWorld;
+    const extractZs = generateExtractFn(zWorld, zLocal, zBeam);
+    functions_to_solve.push((env) => f1(...extractZs(env)));
+
+    const extractXYVariables = generateExtractFn(xBeam, yBeam, xLocal, yLocal, xWorld, yWorld, angle);
+
+    const f2 = (xBeam, yBeam, xLocal, yLocal, xWorld, yWorld, angle) => (xWorld - xBeam) * Math.cos(angle) + (yWorld - yBeam) * Math.sin(angle) - xLocal;
+
+    const f3 = (xBeam, yBeam, xLocal, yLocal, xWorld, yWorld, angle) => -(xWorld - xBeam) * Math.sin(angle) + (yWorld - yBeam) * Math.cos(angle) - yLocal;
+
+    functions_to_solve.push((env) => f2(...extractXYVariables(env)));
+    functions_to_solve.push((env) => f3(...extractXYVariables(env)));
+};
+
+Module.exports = {
+    relateLocalToWorld: relateLocalToWorld,
+    addEqualZconstraint: addEqualZconstraint,
+    addZplus1constraint: addZplus1constraint,
+    fixBeamPosition: fixBeamPosition,
+    linkConnectorectors: linkConnectorectors,
+    fixBeamAngle: fixBeamAngle
+};
--- v2/legobeamtest2.js
+++ v2/legobeamtest2.js
@@ -8,3 +8,14 @@ const g = require('./geometry_relations');
 const printFunctions = () => {
     g.functions_to_solve.map((f) => { console.log(f.toString); });
 };
+
+const buildModel () => {
+    const beamA = new m.Beam(7);
+    const beamB = new m.Beam(5);
+
+    // add six relations
+
+
+
+
+};
