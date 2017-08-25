d328b6dd8916677190c5502079f3ef3296a66274
Khalid <khalid@alum.recurse.com>
2017-08-25 13:10:41 +0200

    Test to see the functions to be solved

--- v2/legobeamtest2.js
+++ v2/legobeamtest2.js
@@ -2,29 +2,9 @@
 
 const m = require('./model2');
 const h = require('./helpers2');
-const d = require('./display2');
 const c = require('./compute');
+const g = require('./geometry_relations');
 
-let functions_to_solve = [];
-
-// let extractZaAndZb = (env) => {
-//     let vZa = c.lookUp(zA, env);
-//     let vZb = c.lookUp(zB, env);
-//     return [vZa, vZb];
-// };
-
-const generateExtractFn = (...variables) =>
-          (env) =>
-              variables.map((v) =>
-                            c.lookUp(v, env));
-
-let addEqualZconstraint = (beamA, beamB) => {
-    const zA = beamA.pos.z;
-    const zB = beamB.pos.z;
-
-    const f = (vZa, vZb) => vZa - vZb;
-
-    const extractZaAndZb = generateExtractFn(zA, zB);
-    functions_to_solve.push((env) => f(...extractZaAndZb(env)));
+const printFunctions = () => {
+    g.functions_to_solve.map((f) => { console.log(f.toString); });
 };
-
