bbb528ed60bbc8f9ff69dcbec2b82b05ecdd640b
Khalid <khalid@alum.recurse.com>
2017-08-25 13:11:55 +0200

    Add modules to test with the REPL

--- v2/testwithrepl.js
+++ v2/testwithrepl.js
@@ -1,3 +1,5 @@
 var myrepl = require("repl").start('$ ');
 myrepl.context['h'] = require('./helpers2');
 myrepl.context['m'] = require('./model2');
+myrepl.context['c'] = require('./compute');
+myrepl.context['l'] = require('./legobeamtest2.js');
