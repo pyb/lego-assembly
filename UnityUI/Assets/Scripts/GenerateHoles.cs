38f4049927d98a19d18eb84ab6acd4976c762557
Khalid <khalid@alum.recurse.com>
2017-09-07 17:56:14 +0200

    Generate holes for a beam

--- UnityUI/Assets/Scripts/GenerateHoles.cs
+++ UnityUI/Assets/Scripts/GenerateHoles.cs
@@ -10,9 +10,14 @@ public class GenerateHoles : MonoBehaviour {
 		GameObject hole;
 		hole = Resources.Load ("Hole") as GameObject;
 		//hole.transform.Translate (20, 0, 0);
-		GameObject entity = Instantiate (hole);
-		entity.transform.parent = this.transform;
-		entity.transform.localPosition = new Vector3 (-0.2f, 0, 0);
+
+		int number = 11;
+
+		for (int i = 0; i < number; i++) {
+			GameObject entity = Instantiate (hole);
+			entity.transform.parent = this.transform;
+			entity.transform.localPosition = new Vector3 (i * -0.2f, 0, 0);
+		}
 	}
 	
 	// Update is called once per frame
