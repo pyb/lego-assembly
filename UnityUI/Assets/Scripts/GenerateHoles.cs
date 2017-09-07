993c984bc7abc66647bff06d54cc653b5cd0e3a6
Khalid <khalid@alum.recurse.com>
2017-09-07 17:47:47 +0200

    Instantiate and transform hole GameObject

--- UnityUI/Assets/Scripts/GenerateHoles.cs
+++ UnityUI/Assets/Scripts/GenerateHoles.cs
@@ -8,11 +8,11 @@ public class GenerateHoles : MonoBehaviour {
 	void Start () {
     	
 		GameObject hole;
-
 		hole = Resources.Load ("Hole") as GameObject;
-
-
-		
+		//hole.transform.Translate (20, 0, 0);
+		GameObject entity = Instantiate (hole);
+		entity.transform.parent = this.transform;
+		entity.transform.localPosition = new Vector3 (-0.2f, 0, 0);
 	}
 	
 	// Update is called once per frame
